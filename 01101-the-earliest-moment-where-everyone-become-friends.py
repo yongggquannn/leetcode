b7d1bcfc268543484e471e1740c3cbcbdde2fcb3
Tan Yong Quan <tanyongquanm@gmail.com>
2025-09-28 21:06:32 +0800

    feat: the earliest moment where everyone become friends #1101

---
+++ 01101-the-earliest-moment-where-everyone-become-friends.py
@@ -0,0 +1,72 @@
+"""
+In a social group, there are N people, with unique integer ids from 0 to N-1.
+
+We have a list of logs, where each logs[i] = [timestamp, id_A, id_B] contains a non-negative integer timestamp, and the ids of two different people.
+
+Each log represents the time in which two different people became friends.  Friendship is symmetric: if A is friends with B, then B is friends with A.
+
+Let's say that person A is acquainted with person B if A is friends with B, or A is a friend of someone acquainted with B.
+
+Return the earliest time for which every person became acquainted with every other person. Return -1 if there is no such earliest time.
+
+
+Input: logs = [[20190101,0,1],[20190104,3,4],[20190107,2,3],[20190211,1,5],[20190224,2,4],[20190301,0,3],[20190312,1,2],[20190322,4,5]], N = 6
+Output: 20190301
+Explanation: 
+The first event occurs at timestamp = 20190101 and after 0 and 1 become friends we have the following friendship groups [0,1], [2], [3], [4], [5].
+The second event occurs at timestamp = 20190104 and after 3 and 4 become friends we have the following friendship groups [0,1], [2], [3,4], [5].
+The third event occurs at timestamp = 20190107 and after 2 and 3 become friends we have the following friendship groups [0,1], [2,3,4], [5].
+The fourth event occurs at timestamp = 20190211 and after 1 and 5 become friends we have the following friendship groups [0,1,5], [2,3,4].
+The fifth event occurs at timestamp = 20190224 and as 2 and 4 are already friend anything happens.
+The sixth event occurs at timestamp = 20190301 and after 0 and 3 become friends we have that all become friends.
+
+Note:
+2 <= N <= 100
+1 <= logs.length <= 10^4
+0 <= logs[i][0] <= 10^9
+0 <= logs[i][1], logs[i][2] <= N - 1
+It's guaranteed that all timestamps in logs[i][0] are different.
+logs are not necessarily ordered by some criteria.
+logs[i][1] != logs[i][2]
+
+"""
+
+from typing import List
+    
+
+def earliestMoment(logs: List[List[int]], n: int) -> int:
+    parent = list(range(n))
+    # Path compression logic using Union Find
+    def find(person):
+        if parent[person] != person:
+            parent[person] = find(parent[person])
+        return parent[person]
+    logs.sort()
+    num_of_groups = n
+    for time_stamp, x, y in logs:
+        x_parent = find(x)
+        y_parent = find(y)
+
+        # Does not require union of the two different group
+        if x_parent == y_parent:
+            continue
+        
+        # Implement union of the two diff. groups
+        parent[x_parent] = y_parent
+
+        num_of_groups -= 1
+        
+        if num_of_groups == 1:
+            return time_stamp
+    
+    return -1
+
+logs = [[20190101,0,1],[20190104,3,4],[20190107,2,3],[20190211,1,5],[20190224,2,4],[20190301,0,3],[20190312,1,2],[20190322,4,5]]
+n = 6
+
+print(earliestMoment(logs, n))
+
+"""
+- Use Union Find data structure to solve the problem
+- Idea of path compression 
+"""
